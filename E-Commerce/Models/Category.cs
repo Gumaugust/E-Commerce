32c45dbdffe2aa33b840e46468748da11fffd0d2
GUMBALL . <xboxrobops2@gmail.com>
2024-12-06 03:06:54 +0700

    Implement S2 Category CRUD Operations

--- E-Commerce/Models/Category.cs
+++ E-Commerce/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace E_Commerce.Models
 {
@@ -6,8 +7,12 @@ namespace E_Commerce.Models
     {
         [Key]
         public int Id { get; set; } 
-        [Required]  
-        public string Name { get; set; }   
+        [Required]
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,100, ErrorMessage ="Vui lòng chọn trong khoảng 1 - 100")]
         public int DisplayOrder { get; set; }   
 
     }
